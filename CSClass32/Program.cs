06ea8cf92f6ae22d8d2ce0b8ad55c6567289b723
Gihun Ham <ham@e-mirim.hs.kr>
2022-05-26 11:47:01 +0900

    #6 클래스 리스트 연습 리스트에서 항목 제거할 때 인덱스 변경 주의

--- CSClass32/Program.cs
+++ CSClass32/Program.cs
@@ -54,6 +54,66 @@ namespace CSClass32
             Product productB = new Product() { name = "소금빵", price=2000};
             Product productC = new Product() { price = 5500, name="당근케이크"};
             Product productD = new Product() { name="보름달" };
+
+            /*
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { name = "함기훈", grade = 1 });
+            students.Add(new Student() { name = "김다흰", grade = 2 });
+            students.Add(new Student() { name = "김미나", grade = 2 });
+            students.Add(new Student() { name = "김민정", grade = 3 });
+            students.Add(new Student() { name = "김세린", grade = 3 });
+            students.Add(new Student() { name = "김소연", grade = 1 });
+            students.Add(new Student() { name = "김지수", grade = 1 });
+            students.Add(new Student() { name = "김지호", grade = 2 });
+            students.Add(new Student() { name = "김찬희", grade = 3 });
+            students.Add(new Student() { name = "김하늘", grade = 2 });
+            students.Add(new Student() { name = "김효리", grade = 1 });
+            */
+            List<Student> students = new List<Student>()
+            {
+                new Student() { grade = 1, name = "함기훈" },
+                new Student() { name = "김다흰", grade = 2 },
+                new Student() { name = "김미나", grade = 2 },
+                new Student() { name = "김민정", grade = 3 },
+                new Student() { name = "김세린", grade = 3 },
+                new Student() { name = "김소연", grade = 1 },
+                new Student() { name = "김지수", grade = 1 },
+                new Student() { name = "김지호", grade = 2 },
+                new Student() { name = "김찬희", grade = 3 },
+                new Student() { name = "김하늘", grade = 2 },
+                new Student() { name = "김효리", grade = 1 },
+            };
+
+            /*
+            foreach (var item in students)
+            {
+                if(item.grade > 2)
+                {
+                    students.Remove(item);
+                }
+            }*/
+            /*
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].grade > 2)
+                {
+                    students.RemoveAt(i);
+                    //students.Remove(students[i]);
+                    i--;
+                }
+            }*/
+            for (int i  = students.Count - 1; i >= 0; i --)
+            {
+                if (students[i].grade > 2)
+                {
+                    students.RemoveAt(i);
+                }
+            }
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item); 
+            }
         }
     }
 }
