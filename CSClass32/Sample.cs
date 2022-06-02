e26642019f93caad3741ade74a5a6e17a47257d6
Gihun Ham <ham@e-mirim.hs.kr>
2022-06-02 12:04:06 +0900

    #12 22-6 생성자 생성자, 정적 생성자

---
+++ CSClass32/Sample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSClass32
+{
+    class Sample
+    {
+        public static int value = 42;
+
+        public Sample()
+        {
+            Console.WriteLine("Sample() 생성자 호출!!");
+        }
+
+        static Sample()
+        {
+            value = 99;
+            Console.WriteLine("Sample() 정적 생성자 호출!!");
+        }
+    }
+}
