884d6a1c8a315359cde1570f4b9b68e70d23ac2e
Gihun Ham <ham@e-mirim.hs.kr>
2022-06-03 11:11:20 +0900

    22-10 메서드 응용(재귀) memorization을 이용해서 효율적으로 재귀 함수를 사용하자.

---
+++ CSClass32/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSClass32
+{
+    class Fibonacci
+    {
+        private static Dictionary<int, long> memo = new Dictionary<int, long>();
+
+        public static long Get(int i)
+        {
+            Console.Write("Get(" + i + ") 호출 -> ");
+            if(i < 0) { return 0; }
+            if(i == 1) {  return 1; }
+            
+            Console.WriteLine();
+            if (memo.ContainsKey(i)) { return memo[i]; }
+            else
+            {
+                return memo[i] = Get(i - 2) + Get(i - 1);
+                
+            }            
+        }
+    }
+}
