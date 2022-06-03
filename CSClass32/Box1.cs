f109094838f64e71b9d5a1ce4a6bc2cdb0c4b98f
Gihun Ham <ham@e-mirim.hs.kr>
2022-06-03 10:44:24 +0900

    22-8 속성(Property) #14 매우 깔끔한 코드를 만들어주는 문법.

---
+++ CSClass32/Box1.cs
@@ -0,0 +1,20 @@
+﻿namespace CSClass32
+{
+    class Box1
+    {
+        public int width;
+        public int height;
+
+        public Box1(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Area()
+        {
+            return this.width * this.height;
+        }
+
+    }
+}
