b056674aa17df6cc4b6d4c17758400b51baa35d7
Gihun Ham <ham@e-mirim.hs.kr>
2022-06-16 11:02:51 +0900

    #23 23-6.상속의 생성자 그리고 클래스 변수 상속 시 주의 점

---
+++ CSClass32/Child.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSClass32
+{
+    public class Child : Parent
+    {
+        public Child() : base(10)
+        {
+            Console.WriteLine("Child():base(10)");
+        }
+        public Child(string input) : base(input)
+        {
+            Console.WriteLine("Child(string input):base(input)");
+        }
+
+        public void CountChiild()
+        {
+            Child.counter++;
+        }
+
+    }
+}
---
+++ CSClass32/Parent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSClass32
+{
+    public class Parent
+    {
+        public static int counter = 0;
+
+        public void CountParent()
+        {
+            Parent.counter++;
+        }
+
+        public Parent()
+        {
+            Console.WriteLine("Parent()");
+        }
+        public Parent(int param)
+        {
+            Console.WriteLine("Parent(int param)");
+        }
+
+        public Parent(string param)
+        {
+            Console.WriteLine("Parent(string param)");
+        }
+
+    }
+}
